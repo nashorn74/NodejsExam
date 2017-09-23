ebd8bd7006e317b17842c538d9f2b3dd2d00f1ed
Jeong kum ho <nashorn74@gmail.com>
2017-09-23 16:38:34 +0900

    EJS, JADE 추가

--- httpExam/request.respons.js
+++ httpExam/request.respons.js
@@ -1,17 +1,39 @@
 var http = require('http');
 var fs = require('fs');
+var ejs = require('ejs');
+var jade = require('jade');
 http.createServer(function(req,res){
 	if (req.method == 'GET') {
 		console.log(req.url+" GET");
-		fs.readFile('index.html', function(err,data) {
-			if (!err) {
-				res.writeHead(200, {'Content-Type':'text/html'});
-				res.end(data);
-			} else {
-				res.writeHead(404);
-				res.end();
-			}
-		});
+		if (req.url == '/') {
+			fs.readFile('index.html', function(err,data) {
+				if (!err) {
+					res.writeHead(200, {'Content-Type':'text/html'});
+					res.end(data);
+				} else {
+					res.writeHead(404); res.end();
+				}
+			});
+		} else if (req.url == '/ejs') {
+			fs.readFile('template.ejs','utf8',function(err,data){
+				if (!err) {
+					var html = ejs.render(data,
+						{name:'Hong',description:'Hello,World for EJS'});
+					res.writeHead(200, {'Content-Type':'text/html'});
+					res.end(html);
+				}
+			});
+		} else if (req.url == '/jade') {
+			fs.readFile('template.jade','utf8',function(err,data){
+				if (!err) {
+					var fn = jade.compile(data);
+					var html = fn({name:'Hong',
+						description:'Hello,World for JADE'});
+					res.writeHead(200,{'Content-Type':'text/html'});
+					res.end(html);
+				}
+			});
+		}	
 	} else if (req.method == 'POST') {
 		console.log(req.url+" POST");
 		req.on('data', function(data) {
