7ade95ae6a9f6220ac2147c3ff32aee13cf04093
Jeong kum ho <nashorn74@gmail.com>
2017-09-30 15:02:14 +0900

    API.html 추가

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname+'/public'));
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
