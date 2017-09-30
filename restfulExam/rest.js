1d94ec8cfe551176848ec38d6369b041c055af76
Jeong kum ho <nashorn74@gmail.com>
2017-09-30 11:06:21 +0900

    RESTful API를 MySQL 연동으로 변경

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -3,70 +3,74 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
-var users = [];
+var mysql = require('mysql');
+var mongodb = require('mongodb');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'test1234',
+  database : 'restful'
+}); 
+connection.connect();
+
 app.get('/user',function(req,res) {
-	res.send(JSON.stringify(users));
+	connection.query('select * from user', 
+		function(err,results,fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(results));
+			}
+		});
 });
 app.get('/user/:id',function(req,res){
-	var select_index = -1;
-	for (var i = 0; i < users.length; i++) {
-		var obj = users[i];
-		if (obj.id == Number(req.params.id)) {
-			select_index = i;
-			break;
-		}
-	}
-	if (select_index == -1) {
-		res.send(JSON.stringify({}));
-	} else {
-		res.send(JSON.stringify(users[select_index]));
-	}
+	connection.query('select * from user where id=?',
+		[req.params.id], function(err, results, fields) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				if (results.length > 0) {
+					res.send(JSON.stringify(results[0]));
+				} else {
+					res.send(JSON.stringify({}));
+				}
+				
+			}
+		});
 });
 app.post('/user',function(req,res){
-	console.log(req.body.name);
-	console.log(req.body.age);
-	var name = req.body.name;
-	var age = Number(req.body.age);
-	var obj = {id:users.length+1,name:name,age:age};
-	users.push(obj);
-	res.send(JSON.stringify({result:true,
-		api:'add user info'}));
+	connection.query(
+		'insert into user(name,age) values(?,?)',
+		[ req.body.name, req.body.age ], 
+		function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		})
 });
 app.put('/user/:id',function(req,res){
-	var select_index = -1;
-	for (var i = 0; i < users.length; i++) {
-		var obj = users[i];
-		if (obj.id == Number(req.params.id)) {
-			select_index = i;
-			break;
-		}
-	}
-	if (select_index == -1) {
-		res.send(JSON.stringify({result:false}));
-	} else {
-		var name = req.body.name;
-		var age = Number(req.body.age);
-		var obj = {id:Number(req.params.id),
-			name:name,age:age};
-		users[select_index] = obj;
-		res.send(JSON.stringify({result:true}));
-	}
+	connection.query(
+		'update user set name=?,age=? where id=?',
+		[ req.body.name, req.body.age, req.params.id ],
+		function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		})
 });
 app.delete('/user/:id',function(req,res){
-	var select_index = -1;
-	for (var i = 0; i < users.length; i++) {
-		var obj = users[i];
-		if (obj.id == Number(req.params.id)) {
-			select_index = i;
-			break;
-		}
-	}
-	if (select_index == -1) {
-		res.send(JSON.stringify({result:false}));
-	} else {
-		users.splice(select_index,1);
-		res.send(JSON.stringify({result:true}));
-	}
+	connection.query('delete from user where id=?',
+		[ req.params.id ], function(err, result) {
+			if (err) {
+				res.send(JSON.stringify(err));
+			} else {
+				res.send(JSON.stringify(result));
+			}
+		});
 });
 app.listen(52273,function() {
 	console.log('Server running');
