5a935ec14d1b0ea3ccd6de4379b6b68d45bd054f
Jeong kum ho <nashorn74@gmail.com>
2017-10-27 14:01:03 +0900

    로그인 기능 추가

--- restfulExam/query.sql
+++ restfulExam/query.sql
@@ -4,6 +4,15 @@ use restful;
 
 create table user(
 id integer primary key auto_increment,
+user_id text,
+password text,
 name text,
 age integer
+);
+
+create table user_login(
+id integer primary key auto_increment,
+user_real_id integer,
+token text,
+created_at DATETIME default current_timestamp
 );
